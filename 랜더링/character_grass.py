a59be2f0f17e0165b69e72bbb59b3337bdb2725f
1820155 <cc3720@naver.com>
2018-09-18 00:10:04 +0900

    fill 부분 수정

--- 랜더링/character_grass.py
+++ 랜더링/character_grass.py
@@ -1,7 +1,14 @@
+
 from pico2d import *
 
 open_canvas()
 
-# fill here
+grass=load_image('grass.png')
+person=load_image('character.png')
+
+grass.draw_now(400,30)
+person.draw_now(400,90)
+
+delay(5)
 
 close_canvas()
