6ff70935cccf24930b030b4c06d00247c4716d93
1820155 <cc3720@naver.com>
2018-10-22 23:38:19 +0900

    ,

--- term/소스코드/main.py
+++ term/소스코드/main.py
@@ -1,52 +1,7 @@
+import game_framework
+import logo_state
 from pico2d import *
 
-
-def handle_events():
-    global running
-    global dir
-    
-    events = get_events()
-
-    for event in events:
-        if event.type == SDL_QUIT:
-            running = False
-        elif event.type == SDL_KEYDOWN:
-            if event.key == SDLK_RIGHT:
-                dir += 1
-            elif event.key == SDLK_LEFT:
-                dir -= 1
-            elif event.key == SDLK_ESCAPE:
-                running == False
-        elif event.type == SDL_KEYUP:
-            if event.key == SDLK_RIGHT:
-                dir -= 1
-            elif event.key == SDLK_LEFT:
-                dir += 1
-
-
 open_canvas()
-a = load_image('토끼run.png')
-b = load_image('토끼runleft.png')
-c = load_image('헤네필드1.png')
-
-
-
-running = True
-x = 400
-frame = 0
-dir = 0
-
-
-while running:
-    clear_canvas()
-    c.draw(400,300)
-    a.clip_draw(frame*100,0,100,100,x,90)
-    
-    update_canvas()
-    
-    handle_events()
-    frame=(frame+1)%8
-    x += dir * 5
-    delay(0.05)
-
+game_framework.run(logo_state)
 close_canvas()
---
+++ term/소스코드/rabbit_state.py
@@ -0,0 +1,54 @@
+from pico2d import *
+import game_framework
+import random
+from enum import Enum
+
+
+def handle_events():
+    global running
+    global dir
+    
+    events = get_events()
+
+    for event in events:
+        if event.type == SDL_QUIT:
+            running = False
+        elif event.type == SDL_KEYDOWN:
+            if event.key == SDLK_RIGHT:
+                dir += 1
+            elif event.key == SDLK_LEFT:
+                dir -= 1
+            elif event.key == SDLK_ESCAPE:
+                running == False
+        elif event.type == SDL_KEYUP:
+            if event.key == SDLK_RIGHT:
+                dir -= 1
+            elif event.key == SDLK_LEFT:
+                dir += 1
+
+
+open_canvas()
+a = load_image('토끼run.png')
+b = load_image('토끼runleft.png')
+c = load_image('헤네필드1.png')
+
+
+running = True
+x = 400
+frame = 0
+dir = 0
+
+
+while running:
+    clear_canvas()
+    c.draw(400,300)
+    a.clip_draw(frame*100,0,100,100,x,90)
+    
+    update_canvas()
+    
+    handle_events()
+    frame=(frame+1)%8
+    x += dir * 5
+    delay(0.05)
+
+close_canvas()
