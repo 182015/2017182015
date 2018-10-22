9428d5446326b96ca79eedf1ed3b5ad8155e2856
1820155 <cc3720@naver.com>
2018-10-22 21:50:34 +0900

    배경 추가

--- term/소스코드/main.py
+++ term/소스코드/main.py
@@ -1,29 +1,52 @@
-#import title_state
-#import game_framework
-
-#from pico2d import *
-
-#open_canvas()
-#game_framework.run(title_state)
-#close_canvas()
-
 from pico2d import *
-import game_framework
-import random
-from enum import Enum
+
+
+def handle_events():
+    global running
+    global dir
+    
+    events = get_events()
+
+    for event in events:
+        if event.type == SDL_QUIT:
+            running = False
+        elif event.type == SDL_KEYDOWN:
+            if event.key == SDLK_RIGHT:
+                dir += 1
+            elif event.key == SDLK_LEFT:
+                dir -= 1
+            elif event.key == SDLK_ESCAPE:
+                running == False
+        elif event.type == SDL_KEYUP:
+            if event.key == SDLK_RIGHT:
+                dir -= 1
+            elif event.key == SDLK_LEFT:
+                dir += 1
+
 
 open_canvas()
-char=load_image('토끼run.png')
+a = load_image('토끼run.png')
+b = load_image('토끼runleft.png')
+c = load_image('헤네필드1.png')
 
-x=0
-frame =0
-while(x<800):
+
+
+running = True
+x = 400
+frame = 0
+dir = 0
+
+
+while running:
     clear_canvas()
-    char.clip_draw(frame*100,0,100,100,x,90)
+    c.draw(400,300)
+    a.clip_draw(frame*100,0,100,100,x,90)
+    
     update_canvas()
+    
+    handle_events()
     frame=(frame+1)%8
-    x+=5
+    x += dir * 5
     delay(0.05)
-    get_events()
 
 close_canvas()
