1cefe00f132a807ea345fa9b35cba7b9846313ff
1820155 <cc3720@naver.com>
2018-11-17 15:08:18 +0900

    배경 움직이는 것 추가

--- term/슈팅게임/grass.py
+++ term/슈팅게임/grass.py
@@ -2,16 +2,27 @@ from pico2d import *
 
 class Grass:
     def __init__(self):
-        self.x =400
-        self.image = load_image('배경.png')
-        
+        self.image1 = load_image('배경1.png')
+        self.image2 = load_image('배경2.png')
+        self.x1 = 800//2
+        self.x2 = 800 + 800/2
+        self.number=0
         
     def do(self):
         pass
         
 
     def draw(self):
-        self.image.clip_draw(100, 0, 800, 600, self.x, 300)
+        self.x1 -= 1
+        self.x2 -= 1
+        if (self.x1 <= -800/2):
+            self.x1 = 800 + 800/2
+        elif (self.x2 <= -800 / 2):
+            self.x2 = 800 + 800 / 2
+            
+        self.image1.clip_draw(0, 0, 800, 600, self.x1, 600/2)
+        self.image2.clip_draw(0, 0, 800, 600, self.x2, 600/2)
+        
             
           
     
